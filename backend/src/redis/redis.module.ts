a9828a17d48e6c147effcc81cab26f1cfa0bee55
Flaip <flaip10@gmail.com>
2025-04-23 04:17:56 +0100

    feat(redis): configure redis module and inject into reservation module

--- backend/src/redis/redis.module.ts
+++ backend/src/redis/redis.module.ts
@@ -2,22 +2,31 @@ import { Module } from '@nestjs/common';
 import {
   RedisModule as NestRedisModule,
   RedisModuleOptions,
+  getRedisConnectionToken,
 } from '@nestjs-modules/ioredis';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisService } from './redis.service';
+import Redis from 'ioredis';
 
 @Module({
   imports: [
     NestRedisModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService): Promise<RedisModuleOptions> =>
-        Promise.resolve({
-          type: 'single',
-          url:
-            configService.get<string>('REDIS_URL') || 'redis://localhost:6379',
-        }),
+      useFactory: (configService: ConfigService): RedisModuleOptions => ({
+        type: 'single',
+        url: configService.get<string>('REDIS_URL') || 'redis://localhost:6379',
+      }),
       inject: [ConfigService],
     }),
   ],
-  exports: [NestRedisModule],
+  providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: (redisClient: Redis): Redis => redisClient,
+      inject: [getRedisConnectionToken()],
+    },
+    RedisService,
+  ],
+  exports: [RedisService],
 })
 export class RedisModule {}
